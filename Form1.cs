c4066b39e315a8a3f7f1901d1761b0ff96518c20
Patrick Sampaio <sampaiofinardi@outlook.com>
2022-09-30 13:13:44 -0300

    Função para buscar arquivo PDF

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,34 @@ namespace exportar_fatura
         {
             InitializeComponent();
         }
+
+        private string GetHTMLFile()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "HTML files (*.html)|*.html";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                return openFileDialog1.FileName;
+            }
+            return textBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = GetHTMLFile();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                return;
+
+
+        }
     }
 }
