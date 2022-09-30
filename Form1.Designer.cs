6c1eb1438ff93624e0b65a7cff6b46cffa38d083
Patrick Sampaio <sampaiofinardi@outlook.com>
2022-09-30 17:28:52 -0300

    Finalização, faltando apenas a parte de "Gerar próximo mês"

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -52,7 +54,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(218, 54);
+            this.button2.Location = new System.Drawing.Point(424, 50);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
@@ -60,12 +62,34 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 92);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(487, 200);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(12, 53);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(113, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Gerar próximo mês";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(511, 87);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(511, 304);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -83,6 +107,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using HtmlAgilityPack;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace exportar_fatura
 {
@@ -17,7 +13,15 @@ namespace exportar_fatura
             InitializeComponent();
         }
 
-        private string GetHTMLFile()
+        private void LogText(string message)
+        {
+            if (richTextBox1.Text == "")
+                richTextBox1.Text += message;
+            else
+                richTextBox1.Text += "\n" + message; 
+        }
+
+        private string GetHTMLFilePath()
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
 
@@ -33,9 +37,91 @@ namespace exportar_fatura
             return textBox1.Text;
         }
 
+        private string GetMonthNumberByName(string name)
+        {
+            switch (name)
+            {
+                case "JAN":
+                    return "01";
+                case "FEV":
+                    return "02";
+                case "MAR":
+                    return "03";
+                case "ABR":
+                    return "04";
+                case "MAI":
+                    return "05";
+                case "JUN":
+                    return "06";
+                case "JUL":
+                    return "07";
+                case "AGO":
+                    return "08";
+                case "SET":
+                    return "09";
+                case "OUT":
+                    return "10";
+                case "NOV":
+                    return "11";
+                default:
+                    return "12";
+            }
+        }
+
+        private HtmlAgilityPack.HtmlDocument GetHTMLDocument(string path)
+        {
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.Load(path);
+            return doc;
+        }
+
+        private HtmlNode GetTableBody(HtmlAgilityPack.HtmlDocument doc)
+        {
+            return doc.DocumentNode.SelectSingleNode("//tbody");
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = GetHTMLFile();
+            textBox1.Text = GetHTMLFilePath();
+        }
+
+        private string GenerateDateColumn(HtmlNode cell)
+        {
+            var cellSpans = cell.Descendants("span").ToList();
+            if (cellSpans.Count <= 0 || cellSpans == null)
+                return null;
+            var month = GetMonthNumberByName(cellSpans[1].InnerText);
+            return DateTime.Parse($"{cellSpans[0].InnerText}/{month}/{DateTime.Now.Year}").ToString("dd/MM/yyyy");
+        }
+
+        private string GenerateValueColumn(HtmlNode cell)
+        {
+            var cellSpans = cell.Descendants("span").ToList();
+            if (cellSpans.Count <= 0 || cellSpans == null)
+                return null;
+            return cellSpans[0].InnerText.Contains("Parcela") ?
+                cellSpans[0].InnerText.Substring(13) :
+                cellSpans[0].InnerText;
+        }
+
+        private string GenerateParcels(HtmlNode cell)
+        {
+            var cellSpans = cell.Descendants("span").ToList();
+            if (cellSpans.Count <= 0 || cellSpans == null)
+                return null;
+
+            if (!cellSpans[0].InnerText.Contains("Parcela"))
+                return string.Empty;
+
+            return cellSpans[0].InnerText.Substring(8, 5);
+        }
+
+        private string GenerateDescriptionColumn(HtmlNode cell)
+        {
+            var cellSpans = cell.Descendants("span").ToList();
+            if (cellSpans.Count <= 0 || cellSpans == null)
+                return null;
+            return cellSpans[0].InnerText;
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -43,7 +129,53 @@ namespace exportar_fatura
             if (textBox1.Text == "")
                 return;
 
+            var doc = GetHTMLDocument(textBox1.Text);
 
+            var tableBody = GetTableBody(doc);
+
+            HtmlNodeCollection tableRows = tableBody.ChildNodes;
+            var lastDate = DateTime.Now;
+
+            foreach (var row in tableRows)
+            {
+                if (row.NodeType == HtmlNodeType.Element &&
+                    !row.InnerText.Contains("PAGTO. POR DEB EM C/C") &&
+                    !row.InnerText.Contains("SALDO ANTERIOR"))
+                {
+                    var i = 0;
+                    HtmlNodeCollection rowCells = row.ChildNodes;
+                    var date = string.Empty;
+                    var desc = string.Empty;
+                    var value = string.Empty;
+                    var parcels = string.Empty;
+                    foreach (var cell in rowCells)
+                    {
+                        if (cell.NodeType == HtmlNodeType.Element && cell.Name == "td")
+                        {
+                            i++;
+                            switch (i)
+                            {
+                                case 1:
+                                    date = GenerateDateColumn(cell);
+                                    if (date == null)
+                                        date = lastDate.ToString("dd/MM/yyyy");
+                                    lastDate = DateTime.Parse(date);
+                                    break;
+                                case 2:
+                                    desc = GenerateDescriptionColumn(cell);
+                                    break;
+                                case 6:
+                                    value = GenerateValueColumn(cell);
+                                    parcels = GenerateParcels(cell);
+                                    break;
+                                default:
+                                    continue;
+                            }
+                        }
+                    }
+                    LogText($"{date}\t{desc} {parcels}\t{value}");
+                }
+            }
         }
     }
 }
